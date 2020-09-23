ff6a47c74f017a41e757c30259cf84724604d7c4
Iminzah <sharroniminzah@gmail.com>
2020-09-23 12:31:56 +0300

    displaying comments

--- app/src/main/java/com/example/postapp/database/PostsAppDatabase.kt
+++ app/src/main/java/com/example/postapp/database/PostsAppDatabase.kt
@@ -1,11 +1,28 @@
-package com.example.postapp.database
+package com.example.postsapp.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.postapp.database.PostsDao
 import com.example.postapp.models.Posts
 
 
-@Database(entities = arrayOf(Posts::class,version=1)
+class PostAppDatabase {
+ @Database(entities = arrayOf(Posts::class), version = 1)
+ abstract class PostAppDatabase: RoomDatabase() {
+  abstract fun postDao(): PostsDao
 
- abstract class PostsAppDatabase {
- abstract  fun PostsDao():PostsDao
+  companion object{
+   private var dbInstance:PostAppDatabase? = null
+
+   fun getDBInstance(context: Context):PostAppDatabase{
+    if(dbInstance==null){
+     dbInstance= Room.databaseBuilder(context,PostAppDatabase::class.java, "postsapp-db").build()
+    }
+    return dbInstance as PostAppDatabase
+   }
+  }
+
+ }
 }
--- app/src/main/java/com/example/postapp/ui/CommentActivity.kt
+++ app/src/main/java/com/example/postapp/ui/CommentActivity.kt
@@ -3,6 +3,43 @@ package com.example.postapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.postapp.R
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.postsapp.repository.CommentsRepository
+import com.example.postsapp.viewmodel.CommentsViewModel
+import com.example.postsapp.viewmodel.CommentsViewModelFactory
+import kotlinx.android.synthetic.main.activity_comments.*
+
+class CommentsActivity : AppCompatActivity() {
+    lateinit var commentsViewModel: CommentsViewModel
+    lateinit var commentsViewModelFactory: CommentsViewModelFactory
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comments)
+
+        val commentsRepository= CommentsRepository()
+        commentsViewModelFactory= CommentsViewModelFactory(commentsRepository)
+        commentsViewModel=
+            ViewModelProvider(this, commentsViewModelFactory).get(CommentsViewModel::class.java)
+
+        commentsViewModel.getPosts()
+
+        commentsViewModel.commentsLiveData.observe(this, Observer { commentsList->
+            rvComments.apply{
+                layoutManager= LinearLayoutManager(this@CommentsActivity)
+                hasFixedSize()
+                adapter=CommentsAdapter(commentsList)
+            }
+            Toast.makeText(baseContext,"${commentsList.size} comments fetched", Toast.LENGTH_LONG).show()
+        })
+        commentsViewModel.commentsFailedLiveData.observe(this, Observer { error->
+            Toast.makeText(baseContext,error, Toast.LENGTH_LONG).show()
+        })
+
+    }
+}
 
 class CommentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
