11657cdd73d2123a6f7d2cdd220c073ebb73b8c6
Iminzah <sharroniminzah@gmail.com>
2020-09-28 10:05:25 +0300

    displaying comments

--- app/src/main/java/com/example/postapp/viewmodels/CommentsViewModel.kt
+++ app/src/main/java/com/example/postapp/viewmodels/CommentsViewModel.kt
@@ -1,4 +1,19 @@
 package com.example.postapp.viewmodels
 
-class CommentsViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+
+class CommentsViewModel(val commentsRepository: CommentsRepository): ViewModeldel() {
+    lateinit var commentsLiveData:LiveData<List<Comments>>
+    var commentsFailedLiveData=MutableliveData<String>()
+
+    fun getAPiComments(postId:Int){
+        viewModelScope.launch{
+            val response=commentsRepository.getComments(postId)
+            if(response.isSuccesful){
+                commentsLiveData.postValue(response.body() as List<Comments>)
+            }
+
+        }
+    }
 }
